4e9fc6082c2cffbe1acb31e2a464392728f78df2
Gabriel Evangelista <gabriel.evanger@gmail.com>
2024-01-02 18:22:27 -0400

    Perguntas Frequentes

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
-// Ativar links do Menu
+// Ativar Links do Menu
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
   const url = location.href;
   const href = link.href;
-
   if (url.includes(href)) {
     link.classList.add("ativo");
   }
@@ -12,7 +11,7 @@ function ativarLink(link) {
 
 links.forEach(ativarLink);
 
-// Ativar itens do orçamento
+// Ativar Items do Orçamento
 
 const parametros = new URLSearchParams(location.search);
 
@@ -24,3 +23,40 @@ function ativarProduto(parametro) {
 }
 
 parametros.forEach(ativarProduto);
+
+// Perguntas Frequentes
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+
+// Galeria de Bicicletas
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
